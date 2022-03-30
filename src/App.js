595aad95d238689f8baaede999d48f5927e63e40
baabyygalll <andiraazzahra19@gmail.com>
2022-03-30 13:32:55 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,90 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+
+  const dataKosong = {
+    namaLengkap : "",
+    email : "",
+    noHandphone : "",
+    latar : "",
+    harapan : ""
+  }
+
+  const [data, setData] = useState(dataKosong)
+
+  const regex = /^[A-Za-z ]*$/
+
+  const [errMsg, setErrMsg] = useState("")
+
+  const fotoSurat = useRef(null)
+
+  const handleInput = e => {
+    const name = e.target.name
+    const value = e.target.value
+
+    if (name === "namaLengkap") {
+      if(regex.test(value)){
+        setErrMsg("")
+      }else {
+        setErrMsg("nama lengkap harus berupa huruf")
+      }
+    }
+
+    setData = ({
+      ...data,
+      [name] : value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    if (errMsg !== "") {
+      alert("Terdapat Data yang Masih Kosong")
+    }else{
+      alert('Data "${data.namaLengkap}" Berhasil Diterima')
+    } e.peventDefault()
+  }
+
+  const reset = () => {
+    setData(dataKosong)
+    setErrMsg("")
+  }
+
   return (
     <div className="App">
       <h1>Pendaftaran Peserta Coding Bootcamp</h1>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Nama Lengkap :</label>
         <input 
         type="text"
         name='nama lengkap'
         className=''
+        value={data.namaLengkap}
+        onChange={handleInput}
+        required
         />
+        <span>{errMsg}</span>
 
         <label>Email :</label>
         <input 
         type="email"
         name='email'
+        value={data.email}
+        onChange={handleInput}
         className=''
+        required
         />
 
         <label>No Handphone :</label>
         <input 
         type="number"
         name='no handphone'
+        value={data.noHandphone}
+        onChange={handleInput}
         className=''
+        required
         />
 
         <label>Latar Belakang Pendidikan :</label>
@@ -33,7 +92,11 @@ function App() {
         type="radio"
         name=' IT'
         className='radio'
+        value={data.latar}
+        onChange={handleInput}
+        required
         /> <label  className='it'>IT</label>
+        <input type="radio" className='radio'/> <label className='it'>Non IT</label>
        
 
         <label for="kelas">Kelas Coding yang Dipilih :</label>
@@ -48,14 +111,19 @@ function App() {
         <input 
         type="file"
         name=''
+        refs={fotoSurat}
         className=''
+        required
         />
 
         <label>Harapan Untuk Coding Bootcamp Ini :</label>
-        <textarea />
+        <textarea
+        value={data.harapan}
+        onChange={handleInput}
+         />
         <br></br>
         <input value="Submit" type="submit" className='submit' />
-        <button>Reset</button>
+        <button onClick={reset}>Reset</button>
       </form>
 
 
